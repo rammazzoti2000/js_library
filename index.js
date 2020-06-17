db4fef6a9ae967d3250f40b76bd26cbc28b7dc0e
Alexandru Bangau <rammazzoti2000@gmail.com>
2020-06-17 15:48:36 +0200

    Add modal window

--- index.js
+++ index.js
@@ -4,57 +4,73 @@ const author = document.getElementById('author');
 const pages = document.getElementById('pages');
 const read = document.getElementById('read');
 const table = document.querySelector('.table');
-const btn = document.getElementById('btn');
+// const btn = document.getElementById('btn');
 
-let myLibrary = [];
-class Book {
-  constructor(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-  }
+const myLibrary = [];
+
+function Book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function remove() {
-  let attr = this.getAttribute('class');  
+  const attr = this.getAttribute('class');
   myLibrary.splice(attr, 1);
   render();
 }
 
-
 function render() {
-  let allBooks = '';
-  
   table.innerHTML = '';
 
-  myLibrary.forEach((book, index) =>  {
+  myLibrary.forEach((book, index) => {
     const movieEl = document.createElement('p');
     movieEl.setAttribute('id', `${index}`);
-    myLibrary[index].read == 'Already read it' ? movieEl.classList.add('green') :  movieEl.classList.add('red'); 
+
+    myLibrary[index].read == 'Already read it' ? movieEl.classList.add('green') :  movieEl.classList.add('red');
 
     const butt = document.createElement('button');
     butt.setAttribute('class', `${index}`);
-    butt.innerHTML = 'Delete';    
+    butt.innerHTML = 'Delete';
     butt.addEventListener('click', remove);
 
     const rusSia = document.createElement('button');
     rusSia.setAttribute('id', `${index}`);
-    rusSia.innerHTML = 'Read?';    
-    rusSia.addEventListener('click', readStatus); 
+    rusSia.innerHTML = 'Read?';
+    rusSia.addEventListener('click', readStatus);
 
-    let text = `${book.title} ${book.author} ${book.pages} ${book.read}`;
-    movieEl.textContent = text;
+    const titleSpan = document.createElement('span');
+    const authorSpan = document.createElement('span');
+    const pagesSpan = document.createElement('span');
+    const readSpan = document.createElement('span');
+
+    const textTtitle = `Title: ${book.title}`;
+    titleSpan.textContent = textTtitle;
+
+    const textAuthor = `Author: ${book.author}`;
+    authorSpan.textContent = textAuthor;
+
+    const textPages = `Pages: ${book.pages}`;
+    pagesSpan.textContent = textPages;
+
+    const textRead = `${book.read}`;
+    readSpan.textContent = textRead;
+
+
+    movieEl.append(titleSpan);
+    movieEl.append(authorSpan);
+    movieEl.append(pagesSpan);
+    movieEl.append(readSpan);
     movieEl.append(butt);
     movieEl.append(rusSia);
     table.append(movieEl);
-    movieEl.addEventListener;   
-  }) 
+  });
 }
 
 function checkBox() {
   if (read.checked) {
-    read.value = "Already read it";
+    read.value = 'Already read it';
   } else {
     read.value = "Haven't read it yet";
   }
@@ -69,10 +85,10 @@ function validate() {
 }
 
 function addBookToLibrary(event) {
-  let valid = validate();
-  if(valid) {
+  const valid = validate();
+  if (valid) {
     checkBox();
-    let book = new Book(title.value, author.value, pages.value, read.value);
+    const book = new Book(title.value, author.value, pages.value, read.value);
     myLibrary.push(book);
     render();
     event.preventDefault();
@@ -82,27 +98,53 @@ function addBookToLibrary(event) {
   }
   event.preventDefault();
   console.log(myLibrary);
-  form.classList.remove('visible');
+  // form.classList.remove('visible');
 }
 
-function showForm() {
-  form.classList.toggle('visible');
-}
+// function showForm() {
+//   form.classList.toggle('visible');
+// }
 
 function readStatus() {
-  let attr = this.getAttribute('id');
-      console.log(this);      
-      if(myLibrary[attr].read == 'Already read it') {
-        myLibrary[attr].read = "Haven't read it yet";
-        this.classList.remove('green');
-        console.log('worked!');
-        render();
-      } else if(myLibrary[attr].read == "Haven't read it yet") {
-        myLibrary[attr].read = "Already read it";
-        this.classList.remove('red');
-        render();
-      }
+  const attr = this.getAttribute('id');
+  console.log(this);
+  if (myLibrary[attr].read === 'Already read it') {
+    myLibrary[attr].read = "Haven't read it yet";
+    this.classList.remove('green');
+    console.log('worked!');
+    render();
+  } else if (myLibrary[attr].read === "Haven't read it yet") {
+    myLibrary[attr].read = 'Already read it';
+    this.classList.remove('red');
+    render();
+  }
 }
 
-btn.addEventListener('click', showForm);
+// btn.addEventListener('click', showForm);
 form.addEventListener('submit', addBookToLibrary);
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("btn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
